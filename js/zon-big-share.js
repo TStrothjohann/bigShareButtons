5d4dd9331e28fa0ea3d8ccfba4547d97f4a8e2f0
Hospitant Entwicklung <thomas@strothjohann.net>
2015-07-21 10:29:53 +0200

    Added check for article-body and inline styles.

--- js/zon-big-share.js
+++ js/zon-big-share.js
@@ -12,15 +12,22 @@
         t: doc.title
     };
 
+    var bigButtonStyles = "<style>.tb-newShareBox{margin: 40px 0;width:100%;}" +
+      ".tb-newShareBox a, .tb-newShareBox a:hover{text-decoration: none; border-bottom: none;}" +
+      ".tb-newShareButton{height: 22px;width: 170px;display: inline-block;color: #FFF;line-height: 1.1;font-size: 15px;font-weight: normal;font-family: 'Arial', 'Verdana', sans;-webkit-border-radius: 3px;-moz-border-radius: 3px;border-radius: 3px;letter-spacing: 0.05em;}" +
+      ".tb-facebook{background: #3b5998 url('http://live0.zeit.de/infografik/bigshare/fb-icon.png') no-repeat scroll 0 1px;margin-right: 25px;padding: 10px 0 5px 40px;}" +
+      ".tb-twitter{background: #55acee url('http://live0.zeit.de/infografik/bigshare/twitter-icon.png') no-repeat scroll 7px 3px;padding: 10px 0 5px 50px;}</style>";
+
     function shareUrl(url) {
         win.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
     }
 
     function addBigShareButtons() {
         var oldShareBox = $('.show_smk'),
+            articleBody = $('.article-body'),
             newShareBox;
 
-        if (!oldShareBox.length) {
+        if (!oldShareBox.length || !articleBody.length) {
             return;
         }
 
@@ -29,7 +36,7 @@
         newShareBox += '<a href="#twitter-share" id="twitter-share"><div class="tb-newShareButton tb-twitter">Twittern</div></a>';
         newShareBox += '</div>'; // close tb-newShareBox
         oldShareBox.css('display', 'none');
-        oldShareBox.before(newShareBox);
+        oldShareBox.before(bigButtonStyles, newShareBox);
         oldShareBox.remove(); // kill old social box
 
         $('#facebook-share').on('click', function() {
